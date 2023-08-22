b878f58872a5d2871e89f3f0dd52bf4769f68893
Wattana Hinchaisri <hanattaw@gmail.com>
2023-08-22 23:10:12 +0700

    add class practices

---
+++ 1-queue.py
@@ -0,0 +1,3 @@
+# we will create a producer coroutine that will generate ten random numbers 
+# and put them on the queue. We will also create a consumer coroutine 
+# that will get numbers from the queue and report their values.
